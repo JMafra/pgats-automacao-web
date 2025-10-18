d93a7179800125aac759a95576241a839285c222
Juliana <jmafra.pe@gmail.com>
2025-10-18 10:32:31 -0300

    Adicionando retry em caso de falha

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  retries: {
+    runMode: 1,
+    openMode: 0,  
+  },
   e2e: {
     setupNodeEvents(on, config) {
     require('cypress-mochawesome-reporter/plugin')(on);
